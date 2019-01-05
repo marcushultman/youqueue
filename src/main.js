b39db9abf726ad2326a0c38c1d413bc81c9fe4d8
Marcus Hultman <marcus.hultman1@gmail.com>
2019-01-05 23:27:38 +0100

    add vue-youtube

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueYoutube from 'vue-youtube'
 import App from './App.vue'
 import router from './router'
 
 Vue.use(VueRouter)
+Vue.use(VueYoutube)
 
 Vue.config.productionTip = false
 
